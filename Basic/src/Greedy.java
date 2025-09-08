66984dba8c1d245e3a508b8ef7cfdc125c107283
Jyoti-Pandeyy <jyotipandeyjyoti48@gmail.com>
2025-09-08 09:15:22 +0530

    Greedy Part 2

--- Basic/src/Greedy.java
+++ Basic/src/Greedy.java
@@ -58,7 +58,20 @@ public class Greedy {
         //     System.out.println(new String(new char[100]).replace('\0', '-'));
         // }
 
-        //4. 
+        // //4. Two City Scheduling
+        // int[][][] inputCosts = {{{10, 20}, {30, 200}, {400, 50}, {30,20}},
+        // {{259,770}, {448,54}, {926,667}, {184,139}, {840,118}, {577,469}},
+        // {{515, 563}, {451, 713}, {537, 709}, {343, 819}, {855, 779},{457, 60}, {650, 359}, {631, 42}},
+        // {{1, 2}, {3, 4}, {5, 6}, {7,8}},
+        // {{1, 2}, {1, 2}, {1, 2}, {1, 2}},
+        // {{10, 100}, {10, 1000}, {50, 500}, {1,100}}};
+        // for(int i=0;i < inputCosts.length;i++){
+        //     System.out.println((i + 1)+ "\tcosts"+ Arrays.deepToString(inputCosts[i]));
+        //     System.out.println("\n\tThe minimum cost to send people equally into city A and B is: "+ twoCityScheduling(inputCosts[i]));
+        //     System.out.println(new String(new char[100]).replace('\0', '-'));
+        // }
+
+        //5. 
 
     }  
 
@@ -159,5 +172,5 @@ public class Greedy {
         return totalCost;
     }
 
-    
+
 }
